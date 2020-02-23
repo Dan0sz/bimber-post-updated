10c760bfc3ef29f79a7ebe60973e46a87ab76161
Daan van den Bergh <daan.van.den.bergh@tig.nl>
2020-02-23 17:42:59 +0100

    Only display modified date if it later than published date

--- bimber-post-updated.php
+++ bimber-post-updated.php
@@ -4,7 +4,7 @@
  * Plugin Name: Post Last Updated for Bimber
  * Plugin URI: https://daan.dev/wordpress/post-last-updated
  * Description: Display 'Last Updated' date along with 'Date Published' in WordPress.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Daan van den Bergh
  * Author URI: https://daan.dev
  * License: GPL2v2 or later
@@ -23,11 +23,24 @@ function daan_add_last_updated_date_time($html, $args)
         return $html;
     }
 
-    $updated_html = sprintf(
-        '<time class="entry-date" datetime="%s" itemprop="dateModified">%s</time>',
-        esc_attr(get_the_modified_date( 'Y-m-d' )),
-        esc_html(the_modified_date(get_option('date_format'), '(' . __('Updated', 'bimber-post-updated') . ': ', ')', false))
-    );
+    $modified_datetime  = get_post_datetime($post->ID, 'modified');
+    $published_datetime = get_post_datetime($post->ID);
+
+    if ($modified_datetime <= $published_datetime) {
+        return $html;
+    }
+
+    $updated_string          = __('Updated', 'bimber-post-updated');
+    $before                  = '(' . $updated_string . ': ';
+    $after                   = ')';
+    $modified_ymd            = $modified_datetime->format('Y-m-d');
+    $modified_human_readable = $modified_datetime->format(get_option('date_format'));
+
+    $updated_html = $before . sprintf(
+            '<time class="entry-date" datetime="%s" itemprop="dateModified">%s</time>',
+            esc_attr($modified_ymd),
+            esc_attr($modified_human_readable)
+        ) . $after;
 
     return $html . $updated_html;
 }
